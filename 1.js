6824385c39716a7f0bf439558f6a50f7710e5774
taufiqtoki <taufiq.toki@gmail.com>
2023-02-07 20:30:02 +0600

    2

---
+++ 1.js
@@ -0,0 +1,4 @@
+function mindGame(n) {
+    return ((n*3)+10)/2-5;
+}
+// console.log(mindGame(33));
---
+++ 2.js
@@ -0,0 +1,9 @@
+function evenOdd(str){
+    if (str.length % 2 == 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
+}
+console.log(evenOdd("Batch7"));
